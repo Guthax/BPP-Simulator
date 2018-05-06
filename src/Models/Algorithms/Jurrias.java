17e393491e46d3256cd50cf36017e3bd1c36d836
Jurriaan <jurrbuitenweg@gmail.com>
2018-05-06 23:10:35 +0200

    Added own Algorithm

--- src/Models/Algorithms/Jurrias.java
+++ src/Models/Algorithms/Jurrias.java
@@ -7,24 +7,34 @@ import Models.HelperClasses.SimulationHandler;
 import Models.Package;
 import com.sun.xml.internal.bind.v2.runtime.reflect.Lister;
 
+import javax.swing.text.html.HTMLDocument;
+import java.lang.reflect.Array;
 import java.util.*;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
 
 public class Jurrias extends Algorithm {
     private String name;
-    static HashMap<Integer, ArrayList<ArrayList<Integer>>> combinationsOfNumbers;
+    private HashMap<Integer, ArrayList<ArrayList<Integer>>> combinationsOfNumbers;
 
+    private  List<Integer> packageSizes;
+    private static int totalNeedOfSum;
+
+    private static ArrayList<Package> packages;
     public Jurrias()
     {
         super("Jurrias");
+        combinationsOfNumbers = new HashMap<Integer, ArrayList<ArrayList<Integer>>>();
     }
 
     @Override
-    public ArrayList<Box> RunAlgorithm(int boxsize, ArrayList<Package> packages) {
+    public ArrayList<Box> RunAlgorithm(int boxsize, ArrayList<Package> packagesp) {
+        packages = new ArrayList<>(packagesp);
         ArrayList<Box> result = new ArrayList<Box>();
         int boxSize = SimulationHandler.simulation.getBoxSize();
 
 
-        HashMap<Integer, Integer> combinations = new HashMap<>();
+        HashMap<Integer, Integer> amountOfcombinations = new HashMap<>();
 
         int totalPackages = Math.round((float)HelperMethods.sumOfPackageList(packages));
 
@@ -33,50 +43,100 @@ public class Jurrias extends Algorithm {
 
         while(totalPackages > 0)
         {
+            while (!doesPackageWithSizeExist(boxSize, packages))
+            {
+                boxSize--;
+            }
             double amount = Math.floor(Double.parseDouble(Float.toString((float)totalPackages / (float)boxSize)));
             totalPackages -= amount * boxSize;
 
             if(amount != 0) {
-                combinations.put(boxSize, (int) (amount));
+                amountOfcombinations.put(boxSize, (int) (amount));
             }
             boxSize--;
         }
 
-        Iterator it = combinations.entrySet().iterator();
-        ArrayList<HashMap<Integer, ArrayList<ArrayList<Integer>>>> res = new ArrayList<HashMap<Integer, ArrayList<ArrayList<Integer>>>>();
+        Iterator it = amountOfcombinations.entrySet().iterator();
+
+        packageSizes = packages.stream().map(p -> p.getSize()).collect(Collectors.toList());
+        while (it.hasNext()) {
+            Map.Entry pair = (Map.Entry)it.next();
+
+            int target = Integer.parseInt(pair.getKey().toString());
+            totalNeedOfSum = Integer.parseInt(pair.getValue().toString());
+
+            sum_up((ArrayList<Integer>) packageSizes, target, totalNeedOfSum);
+
+            //res.add(sum_up(new ArrayList<Integer>(Arrays.asList(numbers)),Integer.parseInt(pair.getKey().toString())));
+
+            it.remove(); // avoids a ConcurrentModificationException8
+        }
+
+        it = combinationsOfNumbers.entrySet().iterator();
 
         while (it.hasNext()) {
             Map.Entry pair = (Map.Entry)it.next();
 
+            int target = Integer.parseInt(pair.getKey().toString());
+
+            for(ArrayList<Integer> alist : (ArrayList<ArrayList<Integer>>)pair.getValue())
+            {
+                Box box = new Box();
+                for(Integer i : alist)
+                {
+                    Package x  = packages.stream()
+                            .filter(s -> s.getSize() == i)
+                            .findAny().orElse(null);
+                    if(x != null) {
+                        packages.remove(x);
+                        box.addPackage(x);
+                    }
+                }
+                if(box.getPackages().size() > 0) {
+                    result.add(box);
+                }
+                //Optional<Package> x  = packages.stream()
+                //        .filter(s -> s.getSize() == 2)
+                //        .findAny();
+               // box.addPackage(packages.);
+            }
             //res.add(sum_up(new ArrayList<Integer>(Arrays.asList(numbers)),Integer.parseInt(pair.getKey().toString())));
 
             it.remove(); // avoids a ConcurrentModificationException
         }
 
-        Integer[] numbers = {3,9,8,4,5,7,10};
-        int target = 5;
-        sum_up(new ArrayList<Integer>(Arrays.asList(numbers)),target);
+        while(packages.size() > 0)
+        {
+            result.addAll(RunAlgorithm(SimulationHandler.simulation.getBoxSize(), packages));
+        }
+
         return result;
     }
 
-    static void sum_up_recursive(ArrayList<Integer> numbers, int target, ArrayList<Integer> partial) {
+    private void sum_up_recursive(ArrayList<Integer> numbers, int target, ArrayList<Integer> partial) {
         int s = 0;
         for (int x: partial) s += x;
         if (s == target) {
-            System.out.println("sum(" + Arrays.toString(partial.toArray()) + ")=" + target);
-            if(combinationsOfNumbers.containsKey(target))
+            if(totalNeedOfSum == 0)
             {
-                combinationsOfNumbers.get(target).add(partial);
+                return;
             }
-            else
-            {
-                combinationsOfNumbers.put(target, new ArrayList<>());
-                combinationsOfNumbers.get(target).add(partial);
+            else {
+                if (combinationsOfNumbers.containsKey(target)) {
+                    totalNeedOfSum--;
+                    combinationsOfNumbers.get(target).add(partial);
+
+                } else {
+                    totalNeedOfSum--;
+                    combinationsOfNumbers.put(target, new ArrayList<>());
+                    combinationsOfNumbers.get(target).add(partial);
+                }
+                packageSizes.remove(partial);
             }
         }
-        if (s >= target) {
-            //return;
-        }
+
+        if (s >= target)
+            return;
         for(int i=0;i<numbers.size();i++) {
             ArrayList<Integer> remaining = new ArrayList<Integer>();
             int n = numbers.get(i);
@@ -85,12 +145,21 @@ public class Jurrias extends Algorithm {
             partial_rec.add(n);
             sum_up_recursive(remaining,target,partial_rec);
         }
-
-        System.out.println("Complete");
     }
-    static void sum_up(ArrayList<Integer> numbers, int target) {
+    private void sum_up(ArrayList<Integer> numbers, int target, int totalNeedOfSum) {
         sum_up_recursive(numbers,target,new ArrayList<Integer>());
     }
 
+    private boolean doesPackageWithSizeExist(int size, ArrayList<Package> packages)
+    {
+        for(Package p : packages)
+        {
+            if(p.getSize() == size)
+            {
+                return true;
+            }
+        }
+        return false;
 
+    }
 }
